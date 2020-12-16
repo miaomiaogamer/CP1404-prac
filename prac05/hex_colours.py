4838a5817903ec24dbadcd68f3436bfec9e78c03
miaomiaogamer <344013468@qq.com>
2020-12-16 21:11:29 +0800

    v1

---
+++ prac05/hex_colours.py
@@ -0,0 +1,15 @@
+def main():
+
+   COLOUR_CODES = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7",
+                "AntiqueWhite1": "#ffefdb", "AntiqueWhite2": "#eedfcc",
+                "AntiqueWhite3": "#cdc0b0", "AntiqueWhite4": "#8b8378",}
+
+   colour_name = input("Enter a colour name: ")
+
+   while colour_name != "":
+
+       print("The code for \"{}\" is {}".format(colour_name,
+                                             COLOUR_CODES.get(colour_name)))
+       colour_name = input("Enter a colour name: ")
+
+main()
